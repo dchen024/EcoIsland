8452c3c29b6244ea479fdc8320d0438bf3577818
adamelsayed06 <adame68@nycstudents.net>
2024-10-20 14:26:15 -0400

    logo change + add nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,35 @@
+"use client";
+
 import LoginButton from "@/components/LoginLogoutButton";
 import UserGreetText from "@/components/UserGreetText";
 import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Example useEffect to check for login status from localStorage
+  useEffect(() => {
+    const userLoggedIn = localStorage.getItem("userToken") ? true : false;
+    setIsLoggedIn(userLoggedIn);
+  }, []);
+
+
   return (
+    <div>
+      <NavBar isLoggedIn={isLoggedIn} />
+    
+    
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <UserGreetText />
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <LoginButton />
-        </div>
       </div>
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
+          src="/earth_logo.PNG"
           alt="Next.js Logo"
           width={180}
           height={37}
@@ -93,5 +107,6 @@ export default function Home() {
         </a>
       </div>
     </main>
+    </div>
   );
 }
